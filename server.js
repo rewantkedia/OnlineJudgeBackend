dfe7562cc84362d06864ef3eb38b96225fb28c79
rewantkedia <kediarewant@gmail.com>
2019-01-29 09:44:39 +0530

    Passport CORS issue fixed

--- server.js
+++ server.js
@@ -30,7 +30,15 @@ app.use(
     saveUninitialized: false //required
   })
 );
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 // Passport
 app.use(passport.initialize());
 app.use(passport.session()); // calls the deserializeUser
